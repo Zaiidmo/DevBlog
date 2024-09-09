4fb97f1657683994c207a2648f597a489ebb3adb
e-lglioui <elgliouiyoucode@gmail.com>
2024-09-09 16:37:47 +0200

    profile-feature migration db

---
+++ config/db.js
@@ -0,0 +1,9 @@
+// const { Sequelize } = require('sequelize');
+
+// // Créez une instance de Sequelize en passant les détails de connexion à la base de données
+// const sequelize = new Sequelize('devblog', 'root', '', {
+//   host: 'localhost',
+//   dialect: 'mysql', // Utilisez 'mysql' pour MySQL
+// });
+
+// module.exports = sequelize;

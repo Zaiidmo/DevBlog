576cf39a4d6146b9eef7d7c966c84353340d4498
Zaiidmo <Zaiidmoumnii@gmail.com>
2024-09-13 17:14:33 +0100

    Update TailwindConfig for fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
       fontFamily: {
         'poppins': 'poppins',
         'logo': 'Kaushan Script',
-        'bruno': 'Bruno Ace',
+        'bruno': 'Bruno Ace SC',
       },
     },
   },
